190c02937b156c0de16fc3221c5bb523a51d2492
delfoxs <140474808+delfoxs@users.noreply.github.com>
2024-03-02 15:32:36 +1030

    Create Human.h

---
+++ Human.h
@@ -0,0 +1,13 @@
+#ifndef HUMAN_H
+#define HUMAN_H
+#include "Player.h"
+using namespace std;
+class Human : public Player {
+    private:
+    string name;
+    public:
+    Human(string name);
+    char makeMove() override;
+    string getName() override;
+};
+#endif
