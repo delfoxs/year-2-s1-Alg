b821acb382d487047c4400c79c10dfd158fa51c6
delfoxs <140474808+delfoxs@users.noreply.github.com>
2024-03-02 15:32:23 +1030

    Create Human.cpp

---
+++ Human.cpp
@@ -0,0 +1,14 @@
+#include "Human.h"
+#include <iostream>
+using namespace std;
+Human::Human(string name) : name(name) {}
+char Human::makeMove() {
+    char move;
+    cout << "Enter move: ";
+    cin >> move;
+    return move;
+}
+
+string Human::getName() {
+    return name;
+}
