849aa80b1c43b3142140400b74313ac7d11d224f
delfoxs <140474808+delfoxs@users.noreply.github.com>
2024-03-10 14:34:06 +1030

    Update Human.cpp

--- Human.cpp
+++ Human.cpp
@@ -1,14 +1,36 @@
 #include "Human.h"
 #include <iostream>
-using namespace std;
-Human::Human(string name) : name(name) {}
-char Human::makeMove() {
-    char move;
-    cout << "Enter move: ";
-    cin >> move;
-    return move;
+#include "Move.h"
+
+Human::Human(std::string name) : name(name) {}
+
+Move* Human::makeMove() {
+    std::string moveName;
+    std::cout << "Enter move: ";
+    std::cin >> moveName;
+
+    if (moveName == "Rock")
+        return new Rock();
+    else if (moveName == "Paper")
+        return new Paper();
+    else if (moveName == "Scissors")
+        return new Scissors();
+    else if (moveName == "Monkey")
+        return new Monkey();
+    else if (moveName == "Robot")
+        return new Robot();
+    else if (moveName == "Pirate")
+        return new Pirate();
+    else if (moveName == "Ninja")
+        return new Ninja();
+    else if (moveName == "Zombie")
+        return new Zombie();
+    else {
+        std::cerr << "Invalid move! Defaulting to Rock." << std::endl;
+        return new Rock();
+    }
 }
 
-string Human::getName() {
+std::string Human::getName() {
     return name;
 }
