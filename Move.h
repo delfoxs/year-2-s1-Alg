fd59f383408b60aba28599e13b0577f5e388a098
delfoxs <140474808+delfoxs@users.noreply.github.com>
2024-03-10 14:41:25 +1030

    Update Move.h

--- Move.h
+++ Move.h
@@ -1,1 +1,52 @@
+#ifndef MOVE_H
+#define MOVE_H
 
+#include <string>
+
+class Move {
+public:
+    virtual std::string getName() = 0;
+    virtual ~Move() {}
+};
+
+class Rock : public Move {
+public:
+    std::string getName() override;
+};
+
+class Paper : public Move {
+public:
+    std::string getName() override;
+};
+
+class Scissors : public Move {
+public:
+    std::string getName() override;
+};
+
+class Monkey : public Move {
+public:
+    std::string getName() override;
+};
+
+class Robot : public Move {
+public:
+    std::string getName() override;
+};
+
+class Pirate : public Move {
+public:
+    std::string getName() override;
+};
+
+class Ninja : public Move {
+public:
+    std::string getName() override;
+};
+
+class Zombie : public Move {
+public:
+    std::string getName() override;
+};
+
+#endif
