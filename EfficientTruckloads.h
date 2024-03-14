d5848afdd82275b4589302b58fa6a331b8524a3d
delfoxs <140474808+delfoxs@users.noreply.github.com>
2024-03-15 01:50:52 +1030

    Update EfficientTruckloads.h

--- EfficientTruckloads.h
+++ EfficientTruckloads.h
@@ -3,18 +3,11 @@
 
 #include <unordered_map>
 
-class EfficientTruckloads {
+class EfficientTruckLoads {
+private:
+    std::unordered_map<int, int> memo;
 public:
     int numTrucks(int numCrates, int loadSize);
-private:
-    std::unordered_map<std::pair<int, int>, int, PairHash> memo;
-    struct PairHash {
-        template <class T1, class T2>
-        std::size_t operator() (const std::pair<T1, T2>& pair) const {
-            return std::hash<T1>()(pair.first) ^ std::hash<T2>()(pair.second);
-        }
-    };
-    int numTrucksMemoized(int numCrates, int loadSize);
 };
 
-#endif
+#endif 
