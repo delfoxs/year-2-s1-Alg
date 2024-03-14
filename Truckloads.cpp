5a27f9c0cd1b02c695b0a59165e7b170ae5b9c6c
delfoxs <140474808+delfoxs@users.noreply.github.com>
2024-03-15 02:17:42 +1030

    Update Truckloads.cpp

--- Truckloads.cpp
+++ Truckloads.cpp
@@ -1,13 +1,11 @@
+
 #include "Truckloads.h"
 
 int Truckloads::numTrucks(int numCrates, int loadSize) {
     if (numCrates <= loadSize) {
         return 1;
     } else {
-        int remainingCrates1 = (numCrates + 1) / 2;
-        int remainingCrates2 = numCrates / 2;
-        int trucksNeeded1 = numTrucks(remainingCrates1, loadSize);
-        int trucksNeeded2 = numTrucks(remainingCrates2, loadSize);
-        return trucksNeeded1 + trucksNeeded2;
+        int trucksNeeded = numTrucks((numCrates + loadSize - 1) / 2, loadSize);
+        return 2 * trucksNeeded;
     }
 }
