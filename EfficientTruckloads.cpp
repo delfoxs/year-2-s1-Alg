f612f8d99ac4e765a0ca4651b51da11051f21589
delfoxs <140474808+delfoxs@users.noreply.github.com>
2024-03-17 16:36:04 +1030

    Update EfficientTruckloads.cpp

--- EfficientTruckloads.cpp
+++ EfficientTruckloads.cpp
@@ -5,15 +5,15 @@ int EfficientTruckloads::numTrucks(int numCrates, int loadSize) {
         return 1;
     }
     
-    if (memo.find(numCrates) != memo.end()) {
-        return memo[numCrates];
+    if (memo.find({numCrates, loadSize}) != memo.end()) { // Check memoization based on both numCrates and loadSize
+        return memo[{numCrates, loadSize}];
     }
     
     int remainingCrates = numCrates - loadSize;
     int trucksNeeded = numTrucks(remainingCrates, loadSize);
     int result = trucksNeeded + 1;
     
-    memo[numCrates] = result;
+    memo[{numCrates, loadSize}] = result;
     
     return result;
 }
